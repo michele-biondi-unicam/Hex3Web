bd070560f3fa392d804574e6c99f0823bd40fe62
Michele Biondi <michele.biondi@studenti.unicam.it>
2017-07-08 21:41:27 +0200

    Refactored some code and bug fix

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,22 +1,16 @@
 // Get the Sidebar
 var mySidebar = document.getElementById("sidebar");
 
-// Get the DIV with overlay effect
-var overlayBg = document.getElementById("overlay-effect");
-
 // Toggle between showing and hiding the sidebar, and add overlay effect
 function w3_open() {
     if (mySidebar.style.display === 'block') {
         mySidebar.style.display = 'none';
-        overlayBg.style.display = "none";
     } else {
         mySidebar.style.display = 'block';
-        overlayBg.style.display = "block";
     }
 }
 
 // Close the sidebar with the close button
 function w3_close() {
     mySidebar.style.display = "none";
-    overlayBg.style.display = "none";
 }
