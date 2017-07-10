ba9953fdb91a2874750865cb0e4e6ee5c6b03d1f
Michele Biondi <michele.biondi@studenti.unicam.it>
2017-07-10 22:23:06 +0200

    Refactoring of node js file

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 // ===== LOAD MODULES ======= //
+
  // express.js server
 var express = require('express');
 var app = express();
+// morgan for logging
 var morgan = require('morgan');
 
 // ===== CONFIGURATION ====== //
 var port = process.env.PORT || 8080; // Defaults to 8080 if environment port not set
 
 // use morgan to log requests to the console
-// COMMENT IT ON RELEASE
+// COMMENT IT OR DELETE ON RELEASE
 app.use(morgan('dev'));
 
 // Loads default page (index.html)
