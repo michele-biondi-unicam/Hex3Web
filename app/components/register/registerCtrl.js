679cea4ac91858f677ebef1268611aa219ae8868
Michele Biondi <michele.biondi@studenti.unicam.it>
2017-07-11 14:41:41 +0200

    Finished implementing the registration forms.

--- app/components/register/registerCtrl.js
+++ app/components/register/registerCtrl.js
@@ -7,7 +7,10 @@ hexTreWebApp.controller('registerCtrl', ['$scope' ,function($scope){
         N.B: if the name or surname is empty it should only display the other. 
     */
     $scope.change_username = function(){
-        if($scope.name == null || $scope.name == ""){
+        if($scope.name == null || $scope.name == "" && $scope.surname == null || $scope.surname == ""){
+            $scope.username = "";
+        }
+        else if($scope.name == null || $scope.name == ""){
             $scope.username = $scope.surname.toLowerCase();
         }else if($scope.surname == null || $scope.surname == ""){
             $scope.username = $scope.name.toLowerCase(); }
