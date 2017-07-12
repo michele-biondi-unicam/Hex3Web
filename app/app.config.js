0a2ebec7bc342b565b4c6273693b0dedf673a973
Michele Biondi <michele.biondi@studenti.unicam.it>
2017-07-12 16:01:24 +0200

    Changed static dirs for security reasons
    An user could previously access folders with passwords and secrets

--- app/app.config.js
+++ app/app.config.js
@@ -5,23 +5,23 @@ hexTreWebApp.config(function($locationProvider, $routeProvider) {
     // ======= AngularJS Routes ========//
     $routeProvider
     .when("/", {
-        templateUrl : "app/components/homepage/homepage.html"
+        templateUrl : "/components/homepage/homepage.html"
     })
     .when("/login", {
-        templateUrl : "app/components/login/login.html"
+        templateUrl : "/components/login/login.html"
     })
     .when("/faq", {
-        templateUrl : "app/components/faq/faq.html"
+        templateUrl : "/components/faq/faq.html"
     })
     .when("/contacts", {
-        templateUrl : "app/components/contacts/contacts.html"
+        templateUrl : "/components/contacts/contacts.html"
     })
     .when("/registerstudent", {
-        templateUrl : "app/components/register/registerStudent.html",
+        templateUrl : "/components/register/registerStudent.html",
         controller : "registerCtrl"
     })
     .when("/registerprofessor", {
-        templateUrl : "app/components/register/registerProfessor.html",
+        templateUrl : "/components/register/registerProfessor.html",
         controller : "registerCtrl"
     });
 });
