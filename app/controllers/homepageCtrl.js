0ed83a9c7ca9be03b64327ed401fef74a4ea29a6
Michele Biondi <michele.biondi@studenti.unicam.it>
2017-07-22 18:50:30 +0200

    Home page template refactoring

--- app/controllers/homepageCtrl.js
+++ app/controllers/homepageCtrl.js
@@ -2,9 +2,9 @@ var hexTreWebApp = angular.module("hexTreWeb");
 
 hexTreWebApp.controller('homepageCtrl', ['$scope','$rootScope',function($scope, $rootScope){
     if($rootScope.authenticated){
-        $scope.template = '/templates/homepage/homepageSignedIn.html';
+        $scope.homePageTemplate = '/templates/homepage/homepageSignedIn.html';
     } else {
-        $scope.template = '/templates/homepage/homepageNotSignedIn.html';
+        $scope.homePageTemplate = '/templates/homepage/homepageNotSignedIn.html';
     }
 
     $scope.token = $rootScope.jwtToken;
