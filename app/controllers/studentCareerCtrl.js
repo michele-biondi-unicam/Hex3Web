db6529d6edd6d982a41a9f5f16ce17ab58a78ba2
Michele Biondi <michele.biondi@studenti.unicam.it>
2017-08-17 11:10:45 +0200

    Added files for student frontend

---
+++ app/controllers/studentCareerCtrl.js
@@ -0,0 +1,1 @@
+var hexTreWebApp = angular.module("hexTreWeb");
---
+++ app/services/student.js
@@ -0,0 +1,5 @@
+var hexTreWebApp = angular.module("hexTreWeb");
+
+hexTreWebApp.service('StudentService', ['$q','$http', function($q, $http){
+    
+}]).run(function(StudentService){});
